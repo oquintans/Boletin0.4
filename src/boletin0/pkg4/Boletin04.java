d0fecf2a9309ea2fa451a7e03a925751120a71dc
oquintansocampo <oquintansocampo@Eniac28.danielcastelao.org>
2014-10-09 11:04:25 +0200

    objetos, terminado.

--- src/boletin0/pkg4/Boletin04.java
+++ src/boletin0/pkg4/Boletin04.java
@@ -3,10 +3,17 @@ package boletin0.pkg4;
 public class Boletin04 {
 
     public static void main(String[] args) {
-        //instancio objeto tipo Semaforo
+        //instancio objeto tipo Semaforo               
         Semaforo sema1=new Semaforo();
-        String valor=sema1.dameColor();
-        System.out.println("color antes de darle un valor: "+valor);
+        String valor=sema1.dameColor();       
+        System.out.println("Color antes de darle un valor: "+valor);
+        
+        sema1.ponerColor("verde");
+        
+        valor=sema1.dameColor();
+        System.out.println("Color : "+valor);
+        //Otra manera
+        System.out.println("Color : "+sema1.dameColor());
         
     
     
