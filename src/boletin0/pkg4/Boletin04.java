60a36a7dbfa00a718994cc006a8f8f86e612f4a0
oquintansocampo <oquintansocampo@Eniac28.danielcastelao.org>
2014-10-09 10:42:07 +0200

    objetos

---
+++ src/boletin0/pkg4/Boletin04.java
@@ -0,0 +1,17 @@
+package boletin0.pkg4;
+
+public class Boletin04 {
+
+    public static void main(String[] args) {
+        //instancio objeto tipo Semaforo
+        Semaforo sema1=new Semaforo();
+        String valor=sema1.dameColor();
+        System.out.println("color antes de darle un valor: "+valor);
+        
+    
+    
+    
+    
+    }
+    
+}
---
+++ src/boletin0/pkg4/Semaforo.java
@@ -0,0 +1,13 @@
+package boletin0.pkg4;
+
+public class Semaforo {
+    //atributos 
+    private String color;
+    //metodos
+    public void ponerColor(String c){
+        color=c;    
+    }
+    public String dameColor(){
+        return color;
+    }
+}
